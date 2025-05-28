06a992471e071fe3b56bb34bb862bfb494a59a55
mohammadreza sharifi <m.sharifi8080@gmail.com>
2025-05-28 19:14:45 +0330

    add data_load function

---
+++ src/data_loader.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+FILE_PATH = r"Divar_house_prediction/data/raw/data_set.csv"
+
+
+def data_load(file_path:str =FILE_PATH)->pd.DataFrame:
+    """
+    Load dataset from a CSV file.
+
+    Args:
+        file_path (str): Path to the CSV file.
+
+    Returns:
+        pd.DataFrame: Loaded DataFrame.
+
+    Raises:
+        FileNotFoundError: If file does not exist.
+    """
+    try:
+        return pd.read_csv(file_path)
+    
+    except FileNotFoundError as e:
+
+        print(f"The data_set not found ! {e}")
+        raise
+
+def main()-> None:
+    print(data_load())
+
+
+if __name__ == "__main__":
+    main()
